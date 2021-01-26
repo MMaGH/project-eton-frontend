7ee97f83c73732e313cbe02b71824f75361ac5fc
MMaGH <misk.mardi@gmail.com>
2021-01-26 09:56:04 +0100

    create task styled_component

---
+++ src/styled_components/Tasks.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const TaskContainer = styled.div`
+
+`
+
+export const TaskCard = styled.div`
+
+`
+
+export const TaskTitle = styled.div`
+
+`
+
+export const TaskTitle = styled.div`
+
+`
+
+export const TaskChech = styled.div`
+
+`
